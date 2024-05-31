09158ce1bbb245498f2d07d830462448e73bc3a1
ArmandSans <armandsans2004@gmail.com>
2024-05-31 11:48:22 +0200

    “Ejercicio1.1”

---
+++ Ejercicio1.1 HolaMundo.c
@@ -0,0 +1,5 @@
+#include <stdio.h> /* Standard Input Output: Necesario para hacer uso de funciones de entrada y salida*/
+
+main() {
+    printf("Hola Mundo\n");  /*Al igual que en python \n sirve para hacer salto de línea*/
+}                            /*La f del printf significa formatted ja que dicha función permite imprimir en un formato específico*/
