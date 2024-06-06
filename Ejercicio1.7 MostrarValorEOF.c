0a89d2d385edcaf295cb23ef85b0b8d2d4d90eaf
ArmandSans <armandsans2004@gmail.com>
2024-06-06 13:50:13 +0200

    Ejercicio1.7

---
+++ Ejercicio1.7 MostrarValorEOF.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+main(){
+    printf("%s %d","Valor de EOF:",EOF); 
+    printf("\n");
+}
+/*Podemos ver que devuelve -1 el cual es un carácter "inválido" 
+cosa que lo distingue del resultado de otras funciones*/
